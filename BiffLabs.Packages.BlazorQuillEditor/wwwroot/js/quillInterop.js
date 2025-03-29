b45e3510f80cc8e303e67b4dfc2b5db592f1be13
Philip Larner <larner6903@gmail.com>
2025-03-29 16:53:34 +0000

    update blob

--- BiffLabs.Packages.BlazorQuillEditor/wwwroot/js/quillInterop.js
+++ BiffLabs.Packages.BlazorQuillEditor/wwwroot/js/quillInterop.js
@@ -1,1 +1,18 @@
-﻿
+﻿window.quillEditor = {
+    editors: {},
+
+    createEditor: function (elementId) {
+        const options = {
+            theme: 'snow'
+        };
+        this.editors[elementId] = new Quill('#' + elementId, options);
+    },
+
+    getEditorContent: function (elementId) {
+        return this.editors[elementId].root.innerHTML;
+    },
+
+    setEditorContent: function (elementId, content) {
+        this.editors[elementId].root.innerHTML = content;
+    }
+};
